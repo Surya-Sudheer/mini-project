d8bb6e677d5295cc3a0c9233b886a051aecdf407
Surya Sudheer <83531525+SuryaSudh@users.noreply.github.com>
2023-05-02 20:56:30 +0530

    updates

--- MaCARE/macares/models.py
+++ MaCARE/macares/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    uid=models.AutoField(primary_key=True)
+    hospital=models.CharField(max_length=50)
+    age=models.IntegerField()
+    yesno=models.CharField(max_length=3)
+    place=models.CharField(max_length=50)
+    wardno=models.IntegerField()
+    phone=models.IntegerField()
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=20,)
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.email
+    
