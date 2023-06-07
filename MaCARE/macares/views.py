b30dcfdbe9975694517ffa14e91e51a9846125fd
Megha <meghacthomas@gmail.com>
2023-06-07 15:25:01 +0530

    asha woman display and home page

--- MaCARE/macares/views.py
+++ MaCARE/macares/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import User
 from datetime import datetime
 
+
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
+
 
 def womenSignUp(request):
     if request.method == 'POST':
@@ -19,9 +21,10 @@ def womenSignUp(request):
         age = request.POST.get('age')
         cmp = request.POST.get('cmp')
 
-        if(password == re_password):
+        if (password == re_password):
             print(occupation)
-            user = User(name=name,email=email,password=password,occupation=occupation,age=age,cmp=cmp,phone=phone,wardno=wardno,district=district)
+            user = User(name=name, email=email, password=password, occupation=occupation,
+                        age=age, cmp=cmp, phone=phone, wardno=wardno, district=district)
             user.save()
             email = [email]
             request.session['uname'] = name
@@ -31,8 +34,23 @@ def womenSignUp(request):
     else:
         return render(request, 'womenSignUp.html')
 
+
 def whome(request):
-    return render(request,'whome.html')
+    if 'email' in request.session:
+        users = User.objects.get(email=request.session['email'])
+        my_dict = {
+            'name': users.name,
+            'email': users.email,
+            'wardno': users.wardno,
+            'district': users.district,
+            'phone': users.phone,
+            'panchayath': users.cmp,
+            'rchid': users.uid
+        }
+        return render(request, 'whome.html', {'my_dict': my_dict})
+    else:
+        data = {'status': 'You need to login first'}
+        return render(request, 'womenSignUp.html', context=data)
 
 
 
@@ -49,9 +67,10 @@ def ashaSignUp(request):
         cmp = request.POST.get('cmp')
         ashaid = request.POST.get('ashaid')
 
-        if(password == re_password):
+        if (password == re_password):
             print(occupation)
-            user = User(name=name,email=email,password=password,occupation=occupation,cmp=cmp,phone=phone,wardno=wardno,district=district,ashaid=ashaid)
+            user = User(name=name, email=email, password=password, occupation=occupation,
+                        cmp=cmp, phone=phone, wardno=wardno, district=district, ashaid=ashaid)
             user.save()
             email = [email]
             request.session['email'] = email
@@ -59,6 +78,7 @@ def ashaSignUp(request):
             return ashahome(request)
     else:
         return render(request, 'ashaSignUp.html')
+
 
 def ashalogin(request):
     if request.method == 'POST':
@@ -69,41 +89,45 @@ def ashalogin(request):
         occupation = user.occupation
         if user.password == password:
             request.session['email'] = email
-            if occupation=='women':
+            if occupation == 'women':
                 return whome(request)
-            elif occupation=='ahaworker':
+            elif occupation == 'ahaworker':
                 return ashahome(request)
         else:
-            data = {'status':"Incorrect Password!!!"}
-            return render(request,'ashalogin.html',context=data)
+            data = {'status': "Incorrect Password!!!"}
+            return render(request, 'ashalogin.html', context=data)
     else:
         return render(request, 'ashalogin.html')
-    
+
+
 def ashahome(request):
-    return render(request,'ashahome.html')
+    return render(request, 'ashahome.html')
+
 
 def wGuideline(request):
-    return render(request,'wGuideline.html')
+    return render(request, 'wGuideline.html')
+
 
 def wremainder(request):
-    return render(request,'wremainder.html')
+    return render(request, 'wremainder.html')
+
 
 def wprofile(request):
     if 'email' in request.session:
-        users  = User.objects.get(email=request.session['email'])
+        users = User.objects.get(email=request.session['email'])
         my_dict = {
             'name': users.name,
-            'email':users.email,
-            'wardno' : users.wardno,
+            'email': users.email,
+            'wardno': users.wardno,
             'district': users.district,
-            'phone':users.phone,
-            'panchayath':users.cmp,
-            'rchid'   : users.uid  
+            'phone': users.phone,
+            'panchayath': users.cmp,
+            'rchid': users.uid
         }
-        return render(request,'wprofile.html',{'my_dict':my_dict})
+        return render(request, 'wprofile.html', {'my_dict': my_dict})
     else:
-        data = {'status':'You need to login first'}
-        return render(request,'womenSignUp.html',context=data)
+        data = {'status': 'You need to login first'}
+        return render(request, 'womenSignUp.html', context=data)
 
 
 def reverse(s):
@@ -116,7 +140,8 @@ def reverse(s):
 
     print(desired_date_str)
     return desired_date_str
-    
+
+
 def reversed(s):
     date_str = s
     original_format = "%Y-%m-%d"
@@ -128,23 +153,24 @@ def reversed(s):
     print(desired_date_str)
     return desired_date_str
 
+
 def wupdate(request):
     if 'email' in request.session:
-        users  = User.objects.get(email=request.session['email'])
+        users = User.objects.get(email=request.session['email'])
         my_dict = {
             'name': users.name,
-            'email':users.email,
-            'wardno' : users.wardno,
+            'email': users.email,
+            'wardno': users.wardno,
             'district': users.district,
-            'phone':users.phone,
-            'panchayath':users.cmp,
-            'rchid'   : users.uid ,
-            'lastmen' : users.lastmen,
-            'lastpg':users.lastpg,
-            'bg':users.bg
-            }
+            'phone': users.phone,
+            'panchayath': users.cmp,
+            'rchid': users.uid,
+            'lastmen': users.lastmen,
+            'lastpg': users.lastpg,
+            'bg': users.bg
+        }
         if request.method == 'POST':
-            users  = User.objects.get(email=request.session['email'])
+            users = User.objects.get(email=request.session['email'])
             print(users.email)
             name = request.POST.get('new_name')
             district = request.POST.get('newdistric')
@@ -155,77 +181,91 @@ def wupdate(request):
             lastpg = request.POST.get('newlastpg')
             bg = request.POST.get('newbg')
             # rchid = request.POST.get('newrchid')
-         
-            print(name,district,panchayath,wardno,phone,lastmen,lastpg,bg)
-            users.name=name
-            users.district=district
-            users.cmp=panchayath
-            users.wardno=wardno
-            users.phone=phone
-            users.lastmen=reverse(lastmen)
-            users.lastpg=reversed(lastpg)  
+
+            print(name, district, panchayath,
+                  wardno, phone, lastmen, lastpg, bg)
+            users.name = name
+            users.district = district
+            users.cmp = panchayath
+            users.wardno = wardno
+            users.phone = phone
+            users.lastmen = reverse(lastmen)
+            users.lastpg = reversed(lastpg)
             # users.lastmen=lastmen
             # users.lastpg=lastpg
-            users.bg=bg
+            users.bg = bg
             users.save()
             return redirect('wprofile')
 
         user = User.objects.get(email=request.session['email'])
-        return render(request,'wupdate.html',{'my_dict':my_dict})
+        return render(request, 'wupdate.html', {'my_dict': my_dict})
     else:
-        data = {'status':'You need to login first'}
-        return render(request,'ashalogin.html',context=data)
+        data = {'status': 'You need to login first'}
+        return render(request, 'ashalogin.html', context=data)
+
 
 def ashaWomenInWard(request):
     users = User.objects.all()
     user_data = []
     if 'email' in request.session:
-        user  = User.objects.get(email=request.session['email'])
+        user = User.objects.get(email=request.session['email'])
         print(user.email)
         for i in users:
-            if i.occupation == "women" and i.wardno==user.wardno:
-                user_data.append({'name':i.name,'wardno':i.wardno})
+            if i.occupation == "women" and i.wardno == user.wardno:
+                user_data.append({'name': i.name,
+                                  'email': i.email,
+                                  'wardno': i.wardno,
+                                  'district': i.district,
+                                  'phone': i.phone,
+                                  'panchayath': i.cmp,
+                                  'rchid': i.uid,
+                                  'lastmen': i.lastmen,
+                                  'lastpg': i.lastpg,
+                                  'bg': i.bg})
         print(user_data)
-        return render(request,'ashaWomenInWard.html',{'user_data':user_data})
+        print("hello hii")
+        
+        return render(request, 'ashaWomenInWard.html', {'user_data': user_data})
     else:
-        data = {'status':'You need to login first'}
-        return render(request,'ashalogin.html',context=data)
-    
+        data = {'status': 'You need to login first'}
+        return render(request, 'ashalogin.html', context=data)
+
 
 def ashaprofile(request):
     if 'email' in request.session:
-        users  = User.objects.get(email=request.session['email'])
+        users = User.objects.get(email=request.session['email'])
         my_dict = {
             'name': users.name,
-            'email':users.email,
-            'wardno' : users.wardno,
+            'email': users.email,
+            'wardno': users.wardno,
             'district': users.district,
-            'phone':users.phone,
-            'panchayath':users.cmp,
-            'rchid'   : users.uid  
+            'phone': users.phone,
+            'panchayath': users.cmp,
+            'rchid': users.uid
         }
-        return render(request,'ashaprofile.html',{'my_dict':my_dict})
+        return render(request, 'ashaprofile.html', {'my_dict': my_dict})
     else:
-        data = {'status':'You need to login first'}
-        return render(request,'ashalogin.html',context=data)
-    
+        data = {'status': 'You need to login first'}
+        return render(request, 'ashalogin.html', context=data)
+
+
 def ashaupdate(request):
     if 'email' in request.session:
-        users  = User.objects.get(email=request.session['email'])
+        users = User.objects.get(email=request.session['email'])
         my_dict = {
             'name': users.name,
-            'email':users.email,
-            'wardno' : users.wardno,
+            'email': users.email,
+            'wardno': users.wardno,
             'district': users.district,
-            'phone':users.phone,
-            'panchayath':users.cmp,
-            'rchid'   : users.uid ,
-            'lastmen' : users.lastmen,
-            'lastpg':users.lastpg,
-            'bg':users.bg
-            }
+            'phone': users.phone,
+            'panchayath': users.cmp,
+            'rchid': users.uid,
+            'lastmen': users.lastmen,
+            'lastpg': users.lastpg,
+            'bg': users.bg
+        }
         if request.method == 'POST':
-            users  = User.objects.get(email=request.session['email'])
+            users = User.objects.get(email=request.session['email'])
             print(users.email)
             name = request.POST.get('new_name')
             district = request.POST.get('newdistric')
@@ -236,57 +276,68 @@ def ashaupdate(request):
             lastpg = request.POST.get('newlastpg')
             bg = request.POST.get('newbg')
             # rchid = request.POST.get('newrchid')
-         
-            print(name,district,panchayath,wardno,phone,lastmen,lastpg,bg)
-            users.name=name
-            users.district=district
-            users.cmp=panchayath
-            users.wardno=wardno
-            users.phone=phone
-             
+
+            print(name, district, panchayath,
+                  wardno, phone, lastmen, lastpg, bg)
+            users.name = name
+            users.district = district
+            users.cmp = panchayath
+            users.wardno = wardno
+            users.phone = phone
+
             # users.lastmen=lastmen
             # users.lastpg=lastpg
-            
+
             users.save()
             return redirect('ashaprofile')
 
         user = User.objects.get(email=request.session['email'])
-        return render(request,'ashaupdate.html',{'my_dict':my_dict})
+        return render(request, 'ashaupdate.html', {'my_dict': my_dict})
     else:
-        data = {'status':'You need to login first'}
-        return render(request,'ashalogin.html',context=data)
+        data = {'status': 'You need to login first'}
+        return render(request, 'ashalogin.html', context=data)
 
 
 def ashaGuidlinesView(request):
-    return render(request,'ashaGuidlinesView.html')
+    return render(request, 'ashaGuidlinesView.html')
+
 
 def start(request):
-    return render(request,'monthdata/start.html')
+    return render(request, 'monthdata/start.html')
+
 
 def month1(request):
-    
-    return render(request,'monthdata/month1.html')
+
+    return render(request, 'monthdata/month1.html')
+
 
 def month2(request):
-    return render(request,'monthdata/month2.html')
+    return render(request, 'monthdata/month2.html')
+
 
 def month3(request):
-    return render(request,'monthdata/month3.html')
+    return render(request, 'monthdata/month3.html')
+
 
 def month4(request):
-    return render(request,'monthdata/month4.html')
+    return render(request, 'monthdata/month4.html')
+
 
 def month5(request):
-    return render(request,'monthdata/month5.html')
+    return render(request, 'monthdata/month5.html')
+
 
 def month6(request):
-    return render(request,'monthdata/month6.html')
+    return render(request, 'monthdata/month6.html')
+
 
 def month7(request):
-    return render(request,'monthdata/month7.html')
+    return render(request, 'monthdata/month7.html')
+
 
 def month8(request):
-    return render(request,'monthdata/month8.html')
+    return render(request, 'monthdata/month8.html')
+
 
 def month9(request):
-    return render(request,'monthdata/month9.html')
+    return render(request, 'monthdata/month9.html')
